5fba43946c33e0ce9f122c3f135ac45fc9d05525
Gabe <poetdreams@hotmail.com>
2016-10-07 01:20:43 +0000

    added custom map

---
+++ callbackMap.js
@@ -0,0 +1,18 @@
+function map(array, newFunction) {
+	array.forEach(function(item) {
+		console.log(item)
+		if (item == "me7 wiksten") {
+			newFunction();
+		}
+	});
+}
+
+function lengthercounter(kick) {
+	console.log("time to go")
+}
+
+// map(["ground", "control", "to", "major", "tom"], (elm) => { return counter.length })
+// map(["ground", "control", "to", "major", "tom"], lengthercounter);
+
+var noob = map(["me7e", "ta7a", "weytk", "me7 wiksten", "nucwem"], lengthercounter);
+console.log(noob);
